e63770852dca4fc81ee3f74cbb402850a74bb3d6
ln-dev7 <leonelngoya@gmail.com>
2021-09-20 23:46:20 +0000

    debut du header et du nav

--- JS/loader.js
+++ JS/loader.js
@@ -1,3 +1,4 @@
 window.addEventListener('load', () => {
     document.querySelector('.loader').classList.add('loader-end');
 })
+
---
+++ JS/toggleMode.js
@@ -0,0 +1,5 @@
+let toggleMode = document.querySelector('.toggle-mode')
+toggleMode.addEventListener('click', ()=>{
+    toggleMode.classList.toggle('active')
+})
+
