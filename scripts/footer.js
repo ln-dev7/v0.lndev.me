bc2180e3cd19275e6b0c230036f6637baf833836
LN <62269693+ln-dev7@users.noreply.github.com>
2022-07-28 16:28:56 +0100

    Update footer.js

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,3 +1,3 @@
 const copyright = document.querySelector('.copyright');
 
-copyright.innerText = `Copyright • Codé par LN • Tout droits réservé • 2021 - ${new Date().getFullYear()}`
+copyright.innerHTML = `Copyright • Coded by <a href="https://www.leonelngoya.com/">LN</a> • All rights reserved • 2021 - ${new Date().getFullYear()}`
