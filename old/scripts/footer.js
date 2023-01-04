5f289c160cf19289862673a72d8cebdb8ce32efd
ln-dev7 <leonelngoya@gmail.com>
2023-01-04 19:31:32 +0100

    update

--- old/scripts/footer.js
+++
@@ -1,3 +0,0 @@
-const copyright = document.querySelector('.copyright');
-
-copyright.innerHTML = `Copyright • Coded by <a href="https://www.leonelngoya.com/">LN</a> • All rights reserved • 2021 - ${new Date().getFullYear()}`
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,14 +5,35 @@ import { AnimatePresence } from "framer-motion";
 import Cursor from "../Components/Cursor/Cursor";
 import MouseContextProvider from "../context/mouseContext";
 
+import GoogleAnalytics from "@bradgarropy/next-google-analytics";
+import SEO from "@bradgarropy/next-seo";
+
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
-    <AnimatePresence initial={false}>
-      <MouseContextProvider>
-        <Cursor />
-        <Header />
-        <Component key={router.pathname} {...pageProps} />
-      </MouseContextProvider>
-    </AnimatePresence>
+    <>
+      <SEO
+        title="Leonel NGOYA"
+        description="
+        I’m a Front-end Developer & Web Integrator, I’m passionate about creating beautiful, intuitive and responsive websites.
+        "
+        keywords={["website", "portfolio", "ln dev", "leonel ngoya"]}
+        icon="/favicon.ico"
+        themeColor="#000000"
+        colorScheme="light"
+        twitter={{
+          image: "/images/lndev.JPG",
+          site: "@ln_dev7",
+          card: "summary",
+        }}
+      />
+      <GoogleAnalytics measurementId="G-E7N5LJTEWP" />
+      <AnimatePresence initial={false}>
+        <MouseContextProvider>
+          <Cursor />
+          <Header />
+          <Component key={router.pathname} {...pageProps} />
+        </MouseContextProvider>
+      </AnimatePresence>
+    </>
   );
 }
