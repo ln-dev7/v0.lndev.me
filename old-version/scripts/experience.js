dcd36a55d1b5b0a05cece02c5a5190abc03c9bf7
ln-dev7 <leonelngoya@gmail.com>
2023-01-07 04:56:21 +0100

    update

--- old-version/scripts/experience.js
+++
@@ -1,6 +0,0 @@
-const faqs = document.querySelectorAll(".faq-card");
-faqs.forEach(faq => {
-  faq.addEventListener('click', () => {
-    faq.classList.toggle('active')
-  })
-});
--- old-version/scripts/loader.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', () => {
-    document.querySelector('.loader').classList.add('loader-end');
-})
-
-
