6b2b5234a35b8f5df97c8691d7628bdf8101814c
ln-dev7 <leonelngoya@gmail.com>
2023-01-07 21:46:13 +0100

    responsive header

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "440f42645a760e1b"; }
+/******/ 		__webpack_require__.h = function() { return "560e7574394dcfd4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
