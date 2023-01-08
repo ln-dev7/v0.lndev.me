92804224d5916cf126794438fc19c92b021ce162
ln-dev7 <leonelngoya@gmail.com>
2023-01-08 12:15:02 +0100

    add header

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5e505ce00e4a089d"; }
+/******/ 		__webpack_require__.h = function() { return "d5566fa606392420"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
