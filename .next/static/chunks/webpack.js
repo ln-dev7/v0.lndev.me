4d3bb210374091982bac6c609f7f80034453f6c2
ln-dev7 <leonelngoya@gmail.com>
2023-01-10 18:29:55 +0100

    update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "aefdf20076f59e82"; }
+/******/ 		__webpack_require__.h = function() { return "f1e2f6e83766dc30"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
