f638f453ddb85c7604eb1cf1a4a57e3658fba3d1
ln-dev7 <leonelngoya@gmail.com>
2023-01-10 21:10:58 +0100

    skills sections

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f1e2f6e83766dc30"; }
+/******/ 		__webpack_require__.h = function() { return "d905231fde12c188"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/skills.tsx
+++ pages/skills.tsx
@@ -3,12 +3,136 @@ import Link from "next/link.js";
 import Image from "next/image";
 
 import { motion } from "framer-motion";
-import { container, item } from "../animation";
+import { container, skillsItem } from "../animation";
 import { useContext } from "react";
 import { MouseContext } from "../context/mouseContext";
 
 export default function Skills() {
   const { cursorChangeHandler } = useContext(MouseContext);
+  const allSkills = [
+    {
+      title: "Web",
+      skills: [
+        {
+          url: "https://www.svgrepo.com/show/452228/html-5.svg",
+          name: "HTML",
+          color: "#E34F26",
+          colorRGB: "227, 79, 38",
+        },
+        {
+          url: "https://www.svgrepo.com/show/452185/css-3.svg",
+          name: "CSS",
+          color: "#1572B6",
+          colorRGB: "21, 114, 182",
+        },
+        {
+          url: "https://www.svgrepo.com/show/354310/sass.svg",
+          name: "SASS",
+          color: "#CC6699",
+          colorRGB: "204, 102, 153",
+        },
+        {
+          url: "https://www.svgrepo.com/show/353925/javascript.svg",
+          name: "JavaScript",
+          color: "#F7DF1E",
+          colorRGB: "247, 223, 30",
+        },
+        {
+          url: "https://www.svgrepo.com/show/354478/typescript-icon.svg",
+          name: "TypeScript",
+          color: "#3178C6",
+          colorRGB: "49, 120, 198",
+        },
+        {
+          url: "https://www.svgrepo.com/show/354259/react.svg",
+          name: "React",
+          color: "#61DAFB",
+          colorRGB: "97, 218, 251",
+        },
+        {
+          url: "https://www.svgrepo.com/show/369457/nextjs.svg",
+          name: "NextJS",
+          color: "#000000",
+          colorRGB: "0, 0, 0",
+        },
+        {
+          url: "https://www.svgrepo.com/show/374109/svelte.svg",
+          name: "Svelte",
+          color: "#FF3E00",
+          colorRGB: "255, 62, 0",
+        },
+        {
+          url: "https://www.svgrepo.com/show/452130/vue.svg",
+          name: "Vue",
+          color: "#4FC08D",
+          colorRGB: "79, 192, 141",
+        },
+        {
+          url: "https://www.svgrepo.com/show/374118/tailwind.svg",
+          name: "TailwindCSS",
+          color: "#38B2AC",
+          colorRGB: "56, 178, 172",
+        },
+        {
+          url: "https://www.svgrepo.com/show/354048/material-ui.svg",
+          name: "MUI",
+          color: "#0081CB",
+          colorRGB: "0, 129, 203",
+        },
+      ],
+    },
+    {
+      title: "Mobile",
+      skills: [
+        {
+          url: "https://www.svgrepo.com/show/373604/flutter.svg",
+          name: "Flutter",
+          color: "#02569B",
+          colorRGB: "2, 86, 155",
+        },
+      ],
+    },
+    {
+      title: "Deployment",
+      skills: [
+        {
+          url: "https://www.svgrepo.com/show/378475/vercel-fill.svg",
+          name: "Vercel",
+          color: "#000000",
+          colorRGB: "0, 0, 0",
+        },
+      ],
+    },
+    {
+      title: "Other",
+      skills: [
+        {
+          url: "https://www.svgrepo.com/show/452210/git.svg",
+          name: "Git",
+          color: "#F05032",
+          colorRGB: "240, 80, 50",
+        },
+        {
+          url: "https://www.svgrepo.com/show/452202/figma.svg",
+          name: "Figma",
+          color: "#F24E1E",
+          colorRGB: "242, 78, 30",
+        },
+        {
+          url: "https://www.svgrepo.com/show/373595/firebase.svg",
+          name: "Firebase",
+          color: "#FFCA28",
+          colorRGB: "255, 202, 40",
+        },
+        {
+          url: "https://www.svgrepo.com/show/374171/vscode.svg",
+          name: "VSCode",
+          color: "#007ACC",
+          colorRGB: "0, 122, 204",
+        },
+      ],
+    },
+  ];
   return (
     <>
       <Head>
@@ -24,7 +148,65 @@ export default function Skills() {
       </Head>
       <main className="skills">
         <div className="page__lines"></div>
-        {/* https://www.svgrepo.com/ */}
+        {allSkills.map((skill, index) => (
+          <motion.div
+            className="skills-section"
+            key={index}
+            variants={container}
+            initial="hidden"
+            animate="show"
+          >
+            <div
+              className="skills-section__title"
+              style={{ overflow: "hidden" }}
+            >
+              <motion.h2
+                animate={{ y: 0, opacity: 1 }}
+                initial={{ y: "50%", opacity: 0 }}
+                transition={{ delay: 0.1, duration: 0.5 }}
+              >
+                {skill.title}
+              </motion.h2>
+            </div>
+            <div className="skills-section__cards">
+              {skill.skills.map((skill, index) => (
+                <div
+                  style={{
+                    overflow: "hidden",
+                  }}
+                >
+                  <motion.div
+                    className="card"
+                    key={index}
+                    variants={skillsItem}
+                  >
+                    <div
+                      className="card-icon"
+                      style={{
+                        backgroundColor: `rgba(${skill.colorRGB}, .1)`,
+                        border: `1px solid ${skill.color}`,
+                      }}
+                    >
+                      <Image
+                        src={skill.url}
+                        alt={skill.name}
+                        width={50}
+                        height={50}
+                      />
+                    </div>
+                    <h3
+                      style={{
+                        color: skill.color,
+                      }}
+                    >
+                      {skill.name}
+                    </h3>
+                  </motion.div>
+                </div>
+              ))}
+            </div>
+          </motion.div>
+        ))}
       </main>
     </>
   );
