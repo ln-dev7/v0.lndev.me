45c5f77e6ae6480226ebf4bfa74d98428f772200
ln-dev7 <leonelngoya@gmail.com>
2023-01-11 09:35:44 +0100

    ajout des certifications

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d905231fde12c188"; }
+/******/ 		__webpack_require__.h = function() { return "eaabfb9dedf969ad"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
