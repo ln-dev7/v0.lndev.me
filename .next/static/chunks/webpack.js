f99b450c81159d2dc60d41e549f238b52e6e81d2
ln-dev7 <leonelngoya@gmail.com>
2023-01-12 22:07:14 +0100

    init projects

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "026165816b1111e5"; }
+/******/ 		__webpack_require__.h = function() { return "8096c252909e9731"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
