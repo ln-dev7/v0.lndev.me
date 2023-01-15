fde379c2dc6c923ed51c6745f530d1cc99cddc1d
ln-dev7 <leonelngoya@gmail.com>
2023-01-15 13:06:04 +0100

    add animation cursor only on chrom

--- Components/Cursor/Cursor.tsx
+++ Components/Cursor/Cursor.tsx
@@ -5,14 +5,41 @@ const Cursor = () => {
   const { cursorType } = useContext(MouseContext);
   const { x, y } = useMousePosition();
 
+  useEffect(() => {
+    if (!(navigator.userAgent.indexOf("Chrome") != -1)) {
+      const mouse1: HTMLElement | null = document.querySelector(".mouse-1");
+      const mouse2: HTMLElement | null = document.querySelector(".mouse-2");
+
+      if (mouse1 && mouse2) {
+        mouse2.style.display = "none";
+        // add a class to the mouse1 element
+        mouse1.classList.add("mouse-1-special");
+      }
+    }
+
+    // if (userAgent.indexOf("Chrome") != -1) {
+    //     return "Google Chrome";
+    // } else if (userAgent.indexOf("Firefox") != -1) {
+    //     return "Mozilla Firefox";
+    // } else if (userAgent.indexOf("Safari") != -1) {
+    //     return "Apple Safari";
+    // } else if (userAgent.indexOf("MSIE") != -1 || userAgent.indexOf("Trident/") != -1) {
+    //     return "Internet Explorer";
+    // } else if (userAgent.indexOf("Edge") != -1) {
+    //     return "Microsoft Edge";
+    // } else {
+    //     return "Unknown";
+    // }
+  }, [cursorType]);
+
   return (
     <>
       <div
-        className={`ring ${cursorType}`}
+        className={`mouse-2 ${cursorType}`}
         style={{ left: `${x}px`, top: `${y}px` }}
       ></div>
       <div
-        className={`dot ${cursorType}`}
+        className={`mouse-1 ${cursorType}`}
         style={{ left: `${x}px`, top: `${y}px` }}
       ></div>
     </>
