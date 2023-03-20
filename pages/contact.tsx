5b8cad688d14ee45c5d3eeb2b4d794c004b7068a
LN <62269693+ln-dev7@users.noreply.github.com>
2023-03-20 14:50:46 +0100

    update

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -118,6 +118,30 @@ export default function Contact() {
                   GitHub
                 </Link>
               </motion.li>
+              <motion.li variants={item}>
+                <Link
+                  href="https://www.awwwards.com/sites/ln-dev"
+                  target="_blank"
+                  rel="noreferrer"
+                  onMouseEnter={() => cursorChangeHandler("hovered")}
+                  onMouseLeave={() => cursorChangeHandler("")}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"
+                    />
+                  </svg>{" "}
+                  Awwwards
+                </Link>
+              </motion.li>
             </ul>
           </motion.div>
           <motion.div
