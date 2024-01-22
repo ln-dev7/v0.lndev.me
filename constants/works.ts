87fb6b65fa917256b6abd640d1f2aeb8ffa5a3e1
LN <leonelngoya@gmail.com>
2024-01-22 15:35:28 +0100

    update works

--- constants/works.ts
+++ constants/works.ts
@@ -6,22 +6,22 @@ interface Work {
 
 export const works: Work[] = [
   {
-    name: "Survaii",
+    name: "Survaii ( 15 / 2024 - Present )",
     url: "https://survaii.io",
     poste: "FrontEnd Developer / NextJS",
   },
   {
-    name: "Gloove",
+    name: "Gloove ( 01 / 2023 - Present )",
     url: "https://gloovee.com/",
     poste: "FrontEnd Developer / NextJS - React Native",
   },
   {
-    name: "LPI",
+    name: "LPI ( 04 / 2023 - 09 / 2023 )",
     url: "https://www.learningplanetinstitute.org/en/",
     poste: "FrontEnd Developer / VueJS",
   },
   {
-    name: "Mboa Digital",
+    name: "Mboa Digital ( 06 / 2022 - 04 / 2023 )",
     url: "https://mboadigital.tech/",
     poste: "FrontEnd Developer / React",
   },
